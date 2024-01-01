f24fe46105d399f128335dd04a9f7deb2e3f78e2
amizzuddin <amizzuddinamin@outlook.com>
2024-01-01 08:41:06 +0000

    RTOS work but does not cortex-debug does not support view from mbed rtos

--- Workspace/mbed1/pyboard_v11/main.cpp
+++ Workspace/mbed1/pyboard_v11/main.cpp
@@ -8,20 +8,34 @@
 // Blinking rate in milliseconds
 #define BLINKING_RATE     100ms
 
+Thread thread; // = Thread(osPriorityNormal, OS_STACK_SIZE, nullptr, "Blue_Led_Thread");
+DigitalOut led(BLUE_LED);
+
+void blue_led_thread()
+{
+    while(true)
+    {
+        ThisThread::sleep_for(1000);
+        led = !led;
+    }
+}
+
 int main()
 {
-    printf("Hello, Mbed!\n");
+    // thread = Thread(osPriorityNormal, OS_STACK_SIZE, nullptr, "Blue_Led_Thread");
+    thread.start(callback(blue_led_thread));
+    // printf("Hello, Mbed!\n");
 
     // Initialise the digital pin LED1 as an output
-    #ifdef BLUE_LED
-        DigitalOut led(BLUE_LED);
-    #else
-        bool led;
-    #endif
+    // #ifdef BLUE_LED
+    //     DigitalOut led(BLUE_LED);
+    // #else
+    //     bool led;
+    // #endif
 
     while (true) {
         printf("Hello, Mbed!\n");
-        led = !led;
+        // led = !led;
         ThisThread::sleep_for(BLINKING_RATE);
     }
 
